96d858ca8f8c2e50a5375259e3ab010b7cf5e92b
evinsdesign <kelvinsaputra024@gmail.com>
2023-11-22 16:59:47 +0800

    Update Javascript inisiasi variable & fungsi session storage, local storage

--- script.js
+++ script.js
@@ -1,2 +1,46 @@
 // inisiasi variable
-// const localTotalVictory = document.getElementById('')
+const localTotalVictory = document.getElementById("local-total-victory-field");
+const localMaximumAttemptField = document.getElementById(
+  "local-maximum-attempt-field"
+);
+const destroyDataButton = document.getElementById("destroy-data-button");
+const playButton = document.getElementById("play-button");
+const beforeGameDisplay = document.getElementById("before-game-display");
+const duringGameDisplay = document.getElementById("during-game-display");
+const afterGameDisplay = document.getElementById("after-game-display");
+const answerButton1 = document.getElementById("answer-1-button");
+const answerButton2 = document.getElementById("answer-2-button");
+const answerButton3 = document.getElementById("answer-3-button");
+const sessionUserAnswerField = document.getElementById(
+  "session-user-answer-field"
+);
+const sessionUserWrongAnswerField = document.getElementById(
+  "session-user-wrong-answer-field"
+);
+const sessionTrueAnswerField = document.getElementById(
+  "session-true-answer-field"
+);
+const sessionUserAttemptsAmountField = document.getElementById(
+  "session-user-attempts-amount-field"
+);
+
+// inisiasi fungsi untuk menghasilkan jawaban permainan;
+function getAnswer() {
+  let answer = "123".split();
+  for (let i = 0; i < answer.length; i++) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let tmp = answer[i];
+    answer[i] = answer[j];
+    answer[j] = tmp;
+  }
+  return answer.join("");
+}
+
+// inisialisasi key session storage
+const sessionAnswerKey = "SESSION_ANSWER";
+const sessionUserAttemptsKey = "SESSION_USER_ATTEMPTS";
+const sessionUserIsPlayingKey = "SESSION_USER_IS_PLAYING";
+
+//inisialisasi key untuk local storage
+const localTotalVictoryKey = "LOCAL_TOTAL_VICTORIES_PLAYED";
+const localMaximumAttemptsKey = "LOCAL_MAXIMUM_ATTEMPTS";
